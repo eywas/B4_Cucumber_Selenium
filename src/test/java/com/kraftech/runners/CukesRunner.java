ec6ed5411326f88798d4c8712d149d5c77963f2a
eywas <umiteywas@gmail.com>
2023-07-18 21:56:21 +0300

    18 temmuz 2003

--- src/test/java/com/kraftech/runners/CukesRunner.java
+++ src/test/java/com/kraftech/runners/CukesRunner.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/kraftech/stepDefinitions"
+        glue = "com/kraftech/stepDefinitions",
+        dryRun = false,  // eğer stepDefinitions içindeki eksik metodu almak istersek burayı
+                        // true yapmalıyız (when metodu)
+                        // burası false olmazsa testlerimizi koşturamayız
+        tags = "@smoke"
 )
 public class CukesRunner {
 }
--- src/test/java/com/kraftech/stepDefinitions/Login_StepDefs.java
+++ src/test/java/com/kraftech/stepDefinitions/Login_StepDefs.java
@@ -9,14 +9,17 @@ public class Login_StepDefs {
     public void the_user_is_on_the_login_page() {
         System.out.println("I open the browser and navigate to kraftech web page");
     }
+
     @When("The user enters teacher credentials")
     public void the_user_enters_teacher_credentials() {
         System.out.println("I enter teacher userEmail and password and click login button");
     }
+
     @Then("The user should be able to login")
     public void the_user_should_be_able_to_login() {
         System.out.println("I verify that url changed dashboard");
     }
+
     @When("The user enters student credentials")
     public void the_user_enters_student_credentials() {
         System.out.println("I enter student userEmail and password and click login button");
@@ -24,6 +27,10 @@ public class Login_StepDefs {
 
     @When("The user enters developer credentials")
     public void theUserEntersDeveloperCredentials() {
+        System.out.println("I enter developer userEmail and password and click login button");
+    }
+    @When("The user enters SDET credentials")
+    public void the_user_enters_sdet_credentials() {
         System.out.println("I enter developer userEmail and password and click login button");
     }
 }
