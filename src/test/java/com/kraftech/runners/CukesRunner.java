505d6b757c6263fb3c7191865a463991a9f245ae
eywas <umiteywas@gmail.com>
2023-07-18 14:57:02 +0300

    17 temmuz 2003 son

---
+++ src/test/java/com/kraftech/runners/CukesRunner.java
@@ -0,0 +1,13 @@
+package com.kraftech.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/kraftech/stepDefinitions"
+)
+public class CukesRunner {
+}
