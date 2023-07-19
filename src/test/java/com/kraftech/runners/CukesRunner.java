5cdd163d29494da4625346464fcee80e55017730
eywas <umiteywas@gmail.com>
2023-07-19 22:24:18 +0300

    19 temmuz 2003

--- src/test/java/com/kraftech/runners/CukesRunner.java
+++ src/test/java/com/kraftech/runners/CukesRunner.java
@@ -6,12 +6,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/kraftech/stepDefinitions",
         dryRun = false,  // eğer stepDefinitions içindeki eksik metodu almak istersek burayı
                         // true yapmalıyız (when metodu)
                         // burası false olmazsa testlerimizi koşturamayız
-        tags = "@smoke"
+        tags = "@parametarizeMethods"
 )
 public class CukesRunner {
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -20,8 +20,8 @@ Feature: Kraftech Login Test
     When The user enters developer credentials
     Then The user should be able to login
 
-  @SDET @login @smoke @dataBase
-  Scenario: Login as a SDET
-    Given The user is on the login page
-    When The user enters SDET credentials
-    Then The user should be able to login
+ # @SDET @login @smoke @dataBase
+  #Scenario: Login as a SDET
+   # Given The user is on the login page
+    #When The user enters SDET credentials
+    #Then The user should be able to login
