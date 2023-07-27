6acb116a06f0574de1f682f0910a74eb9da9a5c5
eywas <umiteywas@gmail.com>
2023-07-27 21:59:49 +0300

    27 temmuz 2023

--- src/test/java/com/kraftech/runners/CukesRunner.java
+++ src/test/java/com/kraftech/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/kraftech/stepDefinitions",
         dryRun = false,
-        tags = "@regression"
+        tags = "@login"
 )
 public class CukesRunner {
 }
